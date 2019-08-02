8f8f4870b9756d5a1d16fdf7eda66312372b0a3d
xlrsonam <sonamgupta68@gmail.com>
2019-08-02 19:05:22 +0530

    Adding more

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=flaskopenssl.py
+FLASK_DEBUG=1
---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+	SECRET_KEY=os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+	PRIVPATH=os.path.join(basedir,'keys/private/')
+	CERTPATH=os.path.join(basedir,'keys/certs/')
+	
